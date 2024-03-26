fe224a49944faa1143fb52874bdeccd9a4f09170
muhammad yahiya <muhammadyahiya.symits@gmail.com>
2024-03-26 12:40:28 +0500

    first comment

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+
+require('dotenv').config()
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+const postsRouter = require('./routes/posts.router')
+const authRouter = require('./routes/auth.router')
+
+app.get("/", (req,res) => res.send("hello"));
+app.use("/api/v1/posts", postsRouter);
+app.use("/api/v1/auth", authRouter)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log("Server is running....")
+})
