39e57f51734e5d009a3e3332f1df1649c74ce336
muhammad yahiya <muhammadyahiya.symits@gmail.com>
2024-03-26 14:04:58 +0500

    fourth commit

--- database/index.js
+++ database/index.js
@@ -5,10 +5,10 @@ const pool = mysql.createPool({
     // user: process.env.DB_USERNAME, 
     // password: process.env.DB_PASSWORD,
     // database: process.env.DB_DBNAME,
+    // waitForConnections: true,
+    // connectionLimit: 10,
+    // queueLimit: 0
     connectionString: process.env.POSTGRES_URL ,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
 });
 
 pool.getConnection((err, conn) => {
