92deb79b2a5a4c14b528983d1a241ae3f0ef6104
muhammad yahiya <muhammadyahiya.symits@gmail.com>
2024-03-26 14:03:29 +0500

    changess

--- database/index.js
+++ database/index.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USERNAME, 
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME,
+    // host: process.env.DB_HOST, 
+    // user: process.env.DB_USERNAME, 
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_DBNAME,
+    connectionString: process.env.POSTGRES_URL ,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
